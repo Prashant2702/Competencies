0fd942ab53a1792ee5c72d57d339abb49a904f5e
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2022-12-30 18:30:17 +0530

    Add files via upload

---
+++ StudentMarks.cpp
@@ -0,0 +1,12 @@
+************************************************
+....Student Marks....
+1.Introduction:
+In this program we are going to print out the marks of the student including their internal and external marks.we are going to use the concept of inheritance into our program.
+2.Expalnation:
+Here in this program we are going to print out the marks obtained by the students in thier internal ,external and total marks ,For that we are going to use out the concept of Multiple inheritance into this program which states that:
+Multiple Inheritance is a feature of C++ where a class can inherit from more than one classes.  The constructors of inherited classes are called in the same order in which they are inherited. For example, in the following program, B’s constructor is called before A’s constructor.A class can be derived from more than one base class.
+Eg: 
+(i) A CHILD class is derived from FATHER and MOTHER class
+(ii) A PETROL class is derived from LIQUID and FUEL class.  
+Here we made two classes named as Internal_marks and External_marks where we are going to put the data by the user and in their derived class there we are going to print out the marks obtained by the student.
+************************************************
