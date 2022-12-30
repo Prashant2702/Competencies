aa74fc5095b8f2adfac20adf70b095ffdbbd993e
Prashant2702 <109785759+Prashant2702@users.noreply.github.com>
2022-12-31 00:26:57 +0530

    Add files via upload

--- StudentMarks.cpp
+++ StudentMarks.cpp
@@ -1,12 +1,64 @@
-************************************************
-....Student Marks....
-1.Introduction:
-In this program we are going to print out the marks of the student including their internal and external marks.we are going to use the concept of inheritance into our program.
-2.Expalnation:
-Here in this program we are going to print out the marks obtained by the students in thier internal ,external and total marks ,For that we are going to use out the concept of Multiple inheritance into this program which states that:
-Multiple Inheritance is a feature of C++ where a class can inherit from more than one classes.  The constructors of inherited classes are called in the same order in which they are inherited. For example, in the following program, B’s constructor is called before A’s constructor.A class can be derived from more than one base class.
-Eg: 
-(i) A CHILD class is derived from FATHER and MOTHER class
-(ii) A PETROL class is derived from LIQUID and FUEL class.  
-Here we made two classes named as Internal_marks and External_marks where we are going to put the data by the user and in their derived class there we are going to print out the marks obtained by the student.
-************************************************
+#include<iostream>
+ using  namespace std;
+ class Internal_marks
+ {
+    protected:
+    int i_marks;
+    public:
+     void get_Internalmarks()
+    {
+        cout<<"enter the internal marks"<< endl;
+        cin>> i_marks;
+    }
+     void put_Internalmarks()
+    {
+        cout<< "internal marks are="<< i_marks << endl;
+        
+    }
+
+ };
+ class External_marks
+ {
+    protected:
+    int e_marks;
+    public:
+    void get_Externalmarks()
+    {
+        cout<<"enter the external marks"<< endl;
+        cin>> e_marks;
+    }
+    void put_Externalmarks()
+    {
+        cout<<" External marks are ="<<e_marks<< endl;
+
+    }
+ 
+ };
+ class student:public Internal_marks,  public External_marks
+ {
+    int roll;
+     char name[30];
+    int total_marks;
+    public:
+    void find_total()
+    {
+        cout<< "enter the rollno. and name of the student"<< endl;
+        cin>> roll;
+        cin>>name;
+        get_Internalmarks();
+        get_Externalmarks();
+        total_marks= i_marks+ e_marks;
+        cout<< roll;
+        cout<< name;
+        put_Internalmarks();
+        put_Externalmarks();
+        cout<< " total marks are "<<"="<< total_marks;
+    }
+
+ };
+ int main()
+ {
+    student s;
+    s.find_total();
+    
+ }
